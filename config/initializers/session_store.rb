99af33eea64991ac4c8a6d5de773d732d511199e
kalendae <taoyge@gmail.com>
2010-09-27 21:50:08 +0000

    rails session expires in 1 hour

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Pinkydata::Application.config.session_store :cookie_store, :key => '_pinkydata_session'
+Pinkydata::Application.config.session_store :cookie_store, :key => '_pinkydata_session', :expire_after => 1.hour
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
